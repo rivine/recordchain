75bea564dab90022e14ebe51b5d425ef6cc0e61b
despiegk <kristof@incubaid.com>
2018-07-23 18:17:56 +0200

    recordchain

--- JumpScale9RecordChain/apps/orderbook/lib/decorators.py
+++
@@ -1,9 +0,0 @@
-def is_logged_in(f):
-    """
-    Check if user has already registered a wallet using valid JWT
-    """
-    def inner(self, *args, **kwargs):
-        if not self.wallet.current:
-            raise RuntimeError('Not-Authorized. Please login first')
-        return f(self, *args, **kwargs)
-    return inner

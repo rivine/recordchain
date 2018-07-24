af68bb6491991d3f8dd4523ed254f16a4841cec7
despiegk <kristof@incubaid.com>
2018-07-24 17:29:10 +0200

    docserver

--- apps/example/test.py
+++ apps/example/test.py
@@ -1,15 +1,13 @@
 from js9 import j
+
 print("[-] starting server")
 
-#TODO, can we autoconfigure client & server, is all for test anyhow
-
 #zerodb 
-cl = j.clients.zdb.testdb_server_start_client_get(start=start)  #starts & resets a zdb in seq mode with name test       
-# db = j.data.bcdb.get(cl)
+cl = j.clients.zdb.testdb_server_start_client_get(start=True)  #starts & resets a zdb in seq mode with name test       
 
 server = j.servers.gedis.configure(host = "localhost", port = "8000", websockets_port = "8001", ssl = False, \
     zdb_instance = "test",
-    secret = "", apps_dir = "", instance='orderbook')
+    secret = "", app_dir = "", instance='example')
 server.start()
 print("[-] server started")
 
@@ -18,4 +16,3 @@ cl = server.client_get()
 from IPython import embed;embed(colors='Linux')
 
 print("** DONE **")
-

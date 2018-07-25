b1b1d0b210d96d3e6bceeabb756ccef79d6252ed
despiegk <kristof@incubaid.com>
2018-07-25 13:25:48 +0200

    docserver

---
+++ apps/chatbot/blueprints/base/static/docsify/get.sh
@@ -0,0 +1,16 @@
+
+set -ex
+
+wget -N unpkg.com/docsify/lib/docsify.min.js
+wget -N unpkg.com/docsify/lib/plugins/search.min.js
+wget -N unpkg.com/prismjs/components/prism-bash.min.js
+wget -N unpkg.com/prismjs/components/prism-python.min.js
+wget -N cdn.jsdelivr.net/npm/echarts@latest/dist/echarts.min.js
+wget -N cdn.jsdelivr.net/npm/docsify-echarts-plugin/lib/index.min.js
+wget -N unpkg.com/docsify-plantuml/dist/docsify-plantuml.min.js
+wget -N unpkg.com/docsify/lib/plugins/disqus.min.js
+wget -N unpkg.com/docsify-themeable
+wget -N cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js
+wget -N unpkg.com/docsify-themeable/dist/css/theme-simple.css
+wget -N unpkg.com/docsify/lib/themes/vue.css
+wget -N cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.css

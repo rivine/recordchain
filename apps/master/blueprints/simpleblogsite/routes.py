69a0c593d117b0417a926c95b34e6a2b8b3e6f87
despiegk <kristof@incubaid.com>
2018-07-26 15:30:45 +0200

    website server

--- apps/master/blueprints/simpleblogsite/routes.py
+++ apps/master/blueprints/simpleblogsite/routes.py
@@ -4,14 +4,25 @@ from js9 import j
 
 login_manager = j.servers.web.latest.loader.login_manager
 
+j.tools.docgenerator.load("https://github.com/Jumpscale/web_libs/tree/master/docsites_examples/simpleblogsite")
+ds = j.tools.docgenerator.docsite_get("simpleblogsite")
+
 @blueprint.route('/')
 def route_default():
     # return redirect(url_for('index_.html'))
     return redirect('/%s/index.html'%name)
 
+@blueprint.route('/blog/<name>.html')
+@blueprint.route('/blog/<name>')
+def route_blog(name):
+    doc = ds.doc_get(name,cat="blog")
+    return render_template('%s_blog.html'%(name),doc=doc,name=name)
 
+
+# ds.doc_get(template)
 # @login_required
 @blueprint.route('/<template>.html')
 def route_template(template):
+    # from IPython import embed;embed(colors='Linux')
     return render_template('%s_%s.html'%(name,template))
 
